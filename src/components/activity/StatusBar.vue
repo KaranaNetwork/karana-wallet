0dc91a6f5c39efdb81134d0ef0428a5ef62c492d
cloud <cloud@xyz.com>
2024-10-19 10:32:42 +0800

    feat: add model

---
+++ src/components/activity/StatusBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="status-bar">
+        <div class="status-icon"></div>
+        <div class="status-text"></div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="less" scoped>
+</style>
---
+++ src/components/modal/DeployDetailModal.vue
@@ -0,0 +1,8 @@
+<template>
+    <a-modal v-model:open="open">    
+    </a-modal>
+</template>
+<script setup lang="ts">
+const open = defineModel('open', { default: false });
+</script>
+<style lang="less" scoped></style>
